ac47b3e126c862b7e3357d14b0d671bb2dfaa4d2
Тимур Файзуллин <aaa.942006@gmail.com>
2026-03-10 18:55:18 +0500

    3-4

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            animaster().fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            animaster().move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animaster().scale(block, 1000, 1.25);
+        });
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            animaster().fadeOut(block, 5000);
         });
 }
 
@@ -25,11 +30,7 @@ function addListeners() {
  * @param element — HTMLElement, который надо анимировать
  * @param duration — Продолжительность анимации в миллисекундах
  */
-// function fadeIn(element, duration) {
-//     element.style.transitionDuration =  `${duration}ms`;
-//     element.classList.remove('hide');
-//     element.classList.add('show');
-// }
+
 
 /**
  * Функция, передвигающая элемент
@@ -71,6 +72,11 @@ function animaster() {
         scale : function(element, duration, ratio) {
             element.style.transitionDuration =  `${duration}ms`;
             element.style.transform = getTransform(null, ratio);
+        },
+        fadeOut : function(element, duration) {
+            element.style.transitionDuration =  `${duration}ms`;
+            element.classList.remove('show');
+            element.classList.add('hide');
         }
     }
 }
